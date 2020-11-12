f52dc9d0a830cfe02345ae965517fe0e3bf5b454
erikamccluskey <71303490+erikamccluskey@users.noreply.github.com>
2020-11-12 13:48:00 -0600

    Delete functs.js
    Not needed

--- functs.js
+++
@@ -1,7 +0,0 @@
-
-
-document.getElementById("addTask").addEventListener("click", addToDo);
-document.getElementById("removeChecked").addEventListener("click", removeAll);
-document.getElementById("newTask").addEventListener("click", lineThrough);
-
-  
